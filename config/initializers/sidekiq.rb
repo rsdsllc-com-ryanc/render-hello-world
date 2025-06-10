21cf6920dcadc49baa688483b7ef579fc20acf32
Ryan Clemens <ryanc@rsdsllc.com>
2025-06-10 17:24:29 -0500

    Added sidekiq ui route

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'], db: ENV['REDIS_DB'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
